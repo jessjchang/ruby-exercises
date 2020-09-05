44a08827ef227e4b404022873e609f21034cc29a
Jess Chang <jesschang77@gmail.com>
2020-09-05 16:10:11 -0700

    Exercises for Variables chapter

---
+++ variables/name.rb
@@ -0,0 +1,14 @@
+# Exercise 1
+puts "Enter your name:"
+name = gets.chomp
+puts "Hello, #{name}!"
+
+# Exercise 3
+10.times { puts name }
+
+# Exercise 4
+puts "Enter your first name:"
+first_name = gets.chomp
+puts "Enter your last name:"
+last_name = gets.chomp
+puts "Your full name is #{first_name} #{last_name}!"
